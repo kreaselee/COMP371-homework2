3fd1b37ad37d38f6776af698ec670408c77cc4de
Kristine <kreaselee@gmail.com>
2021-02-15 00:38:06 -0800

    Set up main activity

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_home = findViewById(R.id.button_home);
+
+        button_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchNextActivity(v);
+            }
+        });
+
+    }
+
+    public void launchNextActivity(View v){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, 1);
     }
 }
