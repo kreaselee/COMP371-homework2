ae664225f5a4f4bf4cb1eda23b589e5d48d7310b
Kristine <kreaselee@gmail.com>
2021-02-18 00:57:15 -0800

    Begin setting up BeerAdapter

---
+++ app/src/main/java/com/example/homework2/BeerAdapter.java
@@ -0,0 +1,15 @@
+package com.example.homework2;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
+
+    private List<Beer> beerList;
+
+    // pass this list into the constructor of the adapter
+    public BeerAdapter(List<Beer> beerList) {
+        this.beerList = beerList;
+    }
+}
