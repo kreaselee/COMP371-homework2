f6ca32d898ff2a8cdb02497d2a6c095e356b3f71
Kristine <kreaselee@gmail.com>
2021-02-18 00:51:54 -0800

    Set up Beer class

---
+++ app/src/main/java/com/example/homework2/Beer.java
@@ -0,0 +1,51 @@
+package com.example.homework2;
+
+public class Beer {
+    // instance variables
+    private String name;
+    private String imageUrl;
+    private String description;
+    private Boolean favorite;
+
+    // constructor
+    public Beer(String name, String imageUrl, String description) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.favorite = false;
+    }
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        this.favorite = favorite;
+    }
+}
