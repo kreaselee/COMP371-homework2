254c275b32f2ce2636951322fa6181bae393fcbe
Kristine <kreaselee@gmail.com>
2021-02-19 22:47:53 -0800

    Adapter updated

--- app/src/main/java/com/example/homework2/BeerAdapter.java
+++ app/src/main/java/com/example/homework2/BeerAdapter.java
@@ -1,19 +1,65 @@
 package com.example.homework2;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
 
-    private List<Beer> beerList;
+    private List<Beer> beers;
 
     // pass this list into the constructor of the adapter
     public BeerAdapter(List<Beer> beerList) {
-        this.beerList = beerList;
+        this.beers = beers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        // inflate the custom layout
+        View beerView = inflater.inflate(R.layout.item_beer, parent, false);
+        // return a new ViewHolder
+        ViewHolder viewHolder = new ViewHolder(beerView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // grab the data model based on the position
+        Beer beer = beers.get(position);
+        // set the view based on the data and the view names
+        holder.textView_name.setText(beer.getName());
+        holder.textView_description.setText(beer.getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
-        // FILL OUT ALL OF THIS 
+        TextView textView_name;
+        TextView textView_description;
+        ImageView imageView_beer;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            // look up views by id
+            textView_name = itemView.findViewById(R.id.textView_name);
+            textView_description = itemView.findViewById(R.id.textView_description);
+            imageView_beer = itemView.findViewById(R.id.imageView_beer);
+            // set on click listener
+        }
     }
 }
