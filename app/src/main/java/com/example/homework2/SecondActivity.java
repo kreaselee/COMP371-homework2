8a1aa76a487ba435c2d5935228a9bc5eed02c662
Kristine <kreaselee@gmail.com>
2021-02-20 00:01:37 -0800

    Second activity updated

--- app/src/main/java/com/example/homework2/SecondActivity.java
+++ app/src/main/java/com/example/homework2/SecondActivity.java
@@ -11,7 +11,6 @@ import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -21,11 +20,12 @@ import cz.msebera.android.httpclient.Header;
 public class SecondActivity extends AppCompatActivity {
 
     private static final String api_url="https://api.punkapi.com/v2/beers?";
+    private String constructed_url;
     private static AsyncHttpClient client = new AsyncHttpClient();
 
     private EditText editText_name;
-    private EditText editText_to;
     private EditText editText_from;
+    private EditText editText_to;
     private Switch switch_highPoint;
     private Boolean highPointCheck = false;
     private Button button_search;
@@ -36,8 +36,8 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         editText_name = findViewById(R.id.editText_name);
-        editText_to = findViewById(R.id.editText_to);
         editText_from = findViewById(R.id.editText_from);
+        editText_to = findViewById(R.id.editText_to);
         switch_highPoint = findViewById(R.id.switch_highPoint);
         button_search = findViewById(R.id.button_search);
 
@@ -66,30 +66,31 @@ public class SecondActivity extends AppCompatActivity {
 
     public void launchNextActivity(View v){
         // get text input and construct url
-        String new_url = api_url;
-        if (editText_name.getText().toString().trim() != "") {
+        constructed_url = api_url;
+        if (!editText_name.getText().toString().trim().matches("")) {
             String name = editText_name.getText().toString().trim();
-            new_url = new_url+"beer_name="+name;
+            constructed_url = constructed_url+"beer_name="+name;
         }
-        if (editText_from.getText().toString().trim() != "") {
+        if (!editText_from.getText().toString().trim().matches("")) {
             String from = editText_from.getText().toString().trim();
-            new_url = new_url+"brewed_after="+from;
+            constructed_url = constructed_url+"brewed_after="+from;
         }
-        if (editText_to.getText().toString().trim() != "") {
+        if (!editText_to.getText().toString().trim().matches("")) {
             String from = editText_to.getText().toString().trim();
-            new_url = new_url+"brewed_before="+from;
+            constructed_url = constructed_url+"brewed_before="+from;
         }
         if (highPointCheck) {
-            new_url = new_url+"abv_gt=3.9";
+            constructed_url = constructed_url+"abv_gt=3.9";
         }
 
         // set the header because of the api endpoint
         client.addHeader("Accept", "application/json");
         // send a get request to the api url
-        client.get(new_url, new AsyncHttpResponseHandler() {
+        client.get(constructed_url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 Log.d("api response", new String(responseBody));
+                // System.out.println(constructed_url);
                 Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
                 startActivity(intent);
             }
