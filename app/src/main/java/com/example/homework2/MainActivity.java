b2c8e3b2b896ada4c68aba5a5553eb08d37172c2
Kristine <kreaselee@gmail.com>
2021-02-21 23:41:59 -0800

    Added image to home

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button_home;
+    private ImageView imageView_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button_home = findViewById(R.id.button_home);
+        imageView_home = findViewById(R.id.imageView_home);
+
+        Picasso.get().load("file:///android_asset/beer.png").into(imageView_home);
 
         button_home.setOnClickListener(new View.OnClickListener() {
             @Override
